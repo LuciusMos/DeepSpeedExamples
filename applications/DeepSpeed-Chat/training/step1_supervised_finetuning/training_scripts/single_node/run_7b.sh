a5f078ed5cb2d70334054debf673f326b663bcac
赵梁煊 <zhaoliangxuan@zhaoliangxuandeMacBook-Pro16.local>
2023-04-23 03:16:19 +0800

    support(zlx): support stage123 bloomz7b

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_7b.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_7b.sh
@@ -9,26 +9,28 @@ if [ "$OUTPUT" == "" ]; then
     OUTPUT=./output
 fi
 if [ "$ZERO_STAGE" == "" ]; then
-    ZERO_STAGE=0
+    ZERO_STAGE=3
 fi
 mkdir -p $OUTPUT
 
 deepspeed main.py \
    --data_path phd_qualified_seeds \
-   --data_split 0,6,4 \
+   --data_split 2,4,4 \
    --model_name_or_path bigscience/bloomz-7b1-mt \
-   --num_padding_at_beginning 1 \
-   --per_device_train_batch_size 8 \
-   --per_device_eval_batch_size 8 \
+   --per_device_train_batch_size 4 \
+   --per_device_eval_batch_size 4 \
    --max_seq_len 512 \
-   --learning_rate 5e-5 \
+   --learning_rate 1e-4 \
    --weight_decay 0.1 \
-   --num_train_epochs 1 \
+   --num_train_epochs 2  \
    --gradient_accumulation_steps 1 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
+   --gradient_checkpointing \
    --zero_stage $ZERO_STAGE \
+   --lora_dim 128 \
+   --lora_module_name decoder.layers. \
    --deepspeed \
    --output_dir $OUTPUT \
    &> $OUTPUT/training.log
